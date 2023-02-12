ce498d92354d2f40f6786f6302d558a13e78e822
viniciussilvestre5 <vinicius@educabiz.com>
2023-02-12 18:21:30 -0300

    Final commit

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity()
@@ -24,7 +39,7 @@ class MainActivity : ComponentActivity()
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CreateBizCard()
                 }
             }
         }
@@ -32,9 +47,127 @@ class MainActivity : ComponentActivity()
 }
 
 @Composable
-fun Greeting(name: String)
+fun CreateBizCard()
 {
-    Text(text = "Hello $name!")
+    val buttonClickedState = remember {mutableStateOf(false)}
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight())
+    {
+        Card(modifier = Modifier
+            .width(0.dp)
+            .height(0.dp)
+            .padding(12.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp))) {
+
+            Column(modifier = Modifier
+                .heightIn(min = 32.dp)
+                .fillMaxWidth(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+
+                CreateImageProfile()
+                Divider()
+                CreateInfo()
+                Button(onClick = {
+                    buttonClickedState.value = !buttonClickedState.value
+                }) {
+                    Text(text = "Portfolio",
+                    style = MaterialTheme.typography.labelLarge)
+                }
+                if (buttonClickedState.value)
+                {
+                    Content()
+                }
+                else
+                {
+                    Box() {
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Content()
+{
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .padding(5.dp))
+    {
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp))) {
+
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3", "Project 4"))
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>)
+{
+    LazyColumn {
+        items(data) {item ->
+            Card(modifier = Modifier
+                .padding(13.dp)
+                .fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
+
+                Row(modifier = Modifier
+                    .padding(8.dp)
+                    .background(MaterialTheme.colorScheme.surface)
+                    .padding(7.dp)
+                    .fillMaxWidth()) {
+                    
+                    CreateImageProfile(modifier = Modifier.size(100.dp))
+                    Column(modifier = Modifier.padding(7.dp).align(alignment = Alignment.CenterVertically)) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great Project", style = MaterialTheme.typography.bodyMedium)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CreateInfo()
+{
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(text = "Vinicius S.", style = MaterialTheme.typography.headlineLarge, color = MaterialTheme.colorScheme.primary)
+        Text(text = "Android Compose Programmer", modifier = Modifier.padding(3.dp))
+        Text(text = "@ViniciusSilves3", modifier = Modifier.padding(3.dp), style = MaterialTheme.typography.titleSmall)
+    }
+}
+
+@Composable
+private fun CreateImageProfile(modifier: Modifier = Modifier)
+{
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(5.dp),
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shape = CircleShape,
+        tonalElevation = 4.dp,
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)) {
+
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "profile image",
+            modifier = modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 @Preview(showBackground = true)
@@ -42,6 +175,6 @@ fun Greeting(name: String)
 fun DefaultPreview()
 {
     JetBizCardTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
